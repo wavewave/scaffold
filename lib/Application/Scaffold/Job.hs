c6bb93d5b189d738471d1050151d77eacc0946d9
Ian-Woo Kim <ianwookim@gmail.com>
2012-11-28 15:02:53 -0500

    compatible with recent version of devadmin. eliminate all bounds of deps in template

--- lib/Application/Scaffold/Job.hs
+++ lib/Application/Scaffold/Job.hs
@@ -1,6 +1,7 @@
 module Application.Scaffold.Job where
 
 import Application.DevAdmin.Config
+import Application.DevAdmin.ProgType
 import Application.Scaffold.Config
 
 import Application.Scaffold.Generate.NewApp
@@ -8,13 +9,15 @@ import Application.Scaffold.Generate.YesodCrud
 
 startMakeYesodCrud :: FilePath -> IO () 
 startMakeYesodCrud configfile = do 
+    b <- constructBuildModes 
     putStrLn "startMakeYesodCrud"
-    withBuildFile $ \(bc,pc) -> 
+    withBuildFile b $ \(bc,pc) -> 
       getYesodCrud configfile >>= maybe (putStrLn "parsing config file error") (createYesodCrud bc)
 
 startMakeApp :: FilePath -> IO () 
-startMakeApp configfile = 
-     withBuildFile $ \(bc,pc) -> 
+startMakeApp configfile = do 
+     b <- constructBuildModes 
+     withBuildFile b $ \(bc,pc) -> 
        getNewApp configfile >>=  maybe (putStrLn "parsing config file error") (createNewApp bc) 
 
 
